d114243209055452a3e07fc0280cc265436dc6ac
Llagus <llagust123@gmail.com>
2020-09-16 10:00:50 +0200

    code commented

--- 1st homework/C_C++.cpp
+++ 1st homework/C_C++.cpp
@@ -6,6 +6,8 @@ Homework 1: Convert a C program to C++
 #include <vector>
 using namespace std; 
 const int lenght = 60;
+
+//create the function sum, that will sum all the elements of a vector
 inline void sum (int& result, int range, vector<int> v){
     result = 0;
     for(int i = 0; i < range; ++i){
@@ -14,13 +16,19 @@ inline void sum (int& result, int range, vector<int> v){
 }
 
 int main(){
+    //initialize variables
     int acum;
     vector<int> numeric_list(lenght); 
-    //create the vector
+    //fill the empty vector
     for(int i = 0; i < lenght; ++i){
         numeric_list[i]=i;
     }
+    //call function sum and display the results
     sum(acum, lenght, numeric_list);
     cout <<"Sum is "<< acum << endl;
     return 0;
 }
+
+/*********************************************************
+end of code. 
+**********************************************************/
