1def098ff235a7992a43b0e300411274390ab9e7
Llagus <llagust123@gmail.com>
2020-09-19 19:41:02 +0200

    creating the files for dijkstra algorithm and graph realitzations

---
+++ 2n leason/dijkstra.cpp
@@ -0,0 +1,24 @@
+/********************************
+ *Dijkstra shortest path algoritm
+ *******************************/ 
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+struct graphEdge{
+    int start_ver, end_ver, cost;
+};
+class Graph{
+
+};
+
+int main (){
+    int V = 5; 
+    vector<int> graph[V];
+
+    return 0; 
+}
+/***************************
+ * End of code
+***************************/
+
