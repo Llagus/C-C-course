42bfcd286c2bf78ee3e4ab8e309491ab9cc9a2bb
Llagus <llagust123@gmail.com>
2020-10-01 09:00:37 +0200

    Corrected spaces

--- 2n leason/dijkstra.cpp
+++ 2n leason/dijkstra.cpp
@@ -14,11 +14,12 @@ class graphEdge{
     }
 };
 
+
 class Node{
     public: 
-        int id; 
+        int id,current_cost; 
         bool visited; 
-        int current_cost; 
+        
         Node(int id = 0, bool visited = false, int current_cost = 0):id(id),visited(visited),current_cost(current_cost){}
 
         bool operator==(Node& node){
@@ -26,6 +27,7 @@ class Node{
             return false;
         }
 };
+
 
 ostream& operator<< (ostream& out,graphEdge& e){
     out<<"Nodes:"<< e.start_ver<<","<< e.end_ver<< "\t cost:" << e.cost<<endl;
@@ -41,6 +43,7 @@ double prob(){
         else prob = prob/360.0; 
     return prob; 
 }
+
 
 class Graph{
     public: 
@@ -59,17 +62,21 @@ class Graph{
                 }
             }
         }
+
         Graph():nodes(0),l_edge(0),graph(0){}//null constructor
+
         //Methods
         int N_nodes(){return this->nodes.size();}
 
         int N_edges(){return this->l_edge.size();}
+
         bool adjacent(int x, int y){
             for(vector<graphEdge>::iterator it = l_edge.begin(); it!=l_edge.end(); ++it){
                 if (it->start_ver==x && it->end_ver == y) return true; 
             }
             return false; 
         }
+
         void neighbors(int x){
             cout<<"The node "<<x<<" has and edge with:";
             for (vector<graphEdge>::iterator it = l_edge.begin(); it!=l_edge.end(); ++it){
@@ -79,6 +86,7 @@ class Graph{
             }
             cout<<endl; 
         }
+
         void add(int node_x, int node_y){
             if (this->adjacent(node_x,node_y)){
                 cout<<"This edge already exist"<<endl;
@@ -88,6 +96,7 @@ class Graph{
                 this->graph[node_x][node_y] = e.cost; 
             }
         }
+
         void del(int node_x, int node_y){
             int i = 0;
             if(this->adjacent(node_x, node_y)){
@@ -104,6 +113,7 @@ class Graph{
                 cout<<"This edge doesn't exist"<<endl;
             }
         }
+
         int get_edge_value(int node_x, int node_y){
             return this->graph[node_x][node_y];
         }
@@ -120,10 +130,7 @@ class Graph{
             this->graph[node_x][node_y] = e.cost; 
         }
 
-
         void print();
-
-
 
     private:
         vector<Node> nodes; 
@@ -131,63 +138,75 @@ class Graph{
         vector<vector<int>> graph; 
 };
 
+
 class PriorityQueue{
     public:
         PriorityQueue():queue(0){}//null constructor
+
         PriorityQueue(Node node){
             this->queue.push_back(node);
         }
-        void chgPriority();
+
+        void chgPriority(int priority){}
         //removes the top element of the queue; 
         void minPriority(){
             this->queue.pop_back(); 
         }
+
         bool contains(Node node){
             Node n;  
             return n == node;
             for(vector<Node>::iterator it = this->queue.begin(); it!=queue.end(); ++it ){
                 if (node == *it) return true;
             }
-
             return false; 
         }
+
         void insert(Node node){
-            for(vector<Node>::iterator it = this->queue.begin(); it!=queue.end(); ++it ){
+            if (this->contains(node)){
+                cout<<"This node is already in the queue"<<endl; 
+            }else{
+                for(vector<Node>::iterator it = this->queue.begin(); it!=queue.end(); ++it ){
                 if (it->current_cost>=node.current_cost)
                 this->queue.emplace(it,node); 
                 break;
-            }
-            
+                }
+            }   
         }
+
         Node top(){
             return this->queue.front(); 
         }
+
         int size(){
             return this->queue.size();
         }
 
     private:
         vector<Node> queue; 
-
 };
+
 
 class ShortestPath{
     public:
         //constructor 
         ShortestPath(Graph* g = 0, int start_node = 0, int end_node= 0):g(g),start_node(start_node),end_node(end_node){}
+
         void vertices(){
             this->g->neighbors(this->start_node);
         }
+
         void path(int node_x, int node_y){
             this->close_set->insert(node_x); 
         }
+
         void path_size(int node_x, int node_y); 
 
     private: 
-    Graph *g = new Graph(); 
-    PriorityQueue *close_set = new PriorityQueue();
-    PriorityQueue *open_set = new PriorityQueue(); 
-    int start_node, end_node; 
+        Graph *g = new Graph(); 
+        PriorityQueue *close_set = new PriorityQueue();
+        PriorityQueue *open_set = new PriorityQueue(); 
+        int start_node, end_node; 
 };
 
 
